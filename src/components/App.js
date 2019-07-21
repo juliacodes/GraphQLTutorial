35b60e5f953d213139aca8020f4950cafc569f2f
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2019-07-21 22:58:57 +0200

    Added LInking and Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,20 @@
 import React from "react";
+import CreateLink from "./CreateLink";
 import LinkList from "./LinkList";
-
+import Header from "./Header";
+import { Switch, Route } from "react-router-dom";
 function App() {
-  return <LinkList />;
+  return (
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+        </Switch>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./index.css";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
@@ -6,7 +7,7 @@ import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
-
+import { BrowserRouter } from "react-router-dom";
 const httpLink = createHttpLink({
   uri: "http://localhost:4000"
 });
@@ -17,9 +18,11 @@ const client = new ApolloClient({
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
