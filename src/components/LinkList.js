d0de754f56049bdf90f817dc6fc10adbfd479685
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2019-07-22 20:33:06 +0200

    Added ability to vote

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -11,6 +11,16 @@ const FEED_QUERY = gql`
         createdAt
         url
         description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
@@ -28,8 +38,8 @@ class LinkList extends Component {
 
           return (
             <div>
-              {linksToRender.map(link => (
-                <Link key={link.id} link={link} />
+              {linksToRender.map((link, index) => (
+                <Link key={link.id} link={link} index={index} />
               ))}
             </div>
           );
